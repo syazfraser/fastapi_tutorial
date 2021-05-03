d38a2feefdd10d09ff1a6a1dc9272d2797264893
Syazwan Rifdi <syazwanrifdi@xsquare.sg>
2021-05-03 12:49:17 +0800

    Exceptions and status code

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -17,7 +17,7 @@ def get_db():
         db.close()
 
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -32,7 +32,14 @@ def get_all_blogs(db: Session = Depends(get_db)):
     return blogs
 
 
-@app.get('/blog/{id}')
-def get_single_blog(id, db: Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code=200)
+def get_single_blog(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    return blo
+    if not blog:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Blog {id} is not available.'
+        )
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': f'Blog {id} is not available'}
+    return blog
