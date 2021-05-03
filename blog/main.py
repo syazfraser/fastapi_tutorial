d541f23d8be6f8d03115e7723a7b65ce8a5b130d
Syazwan Rifdi <syazwanrifdi@xsquare.sg>
2021-05-03 13:35:28 +0800

    Trying out update operation

--- blog/main.py
+++ blog/main.py
@@ -3,7 +3,6 @@ from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
 
-
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
@@ -28,9 +27,28 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Blog {id} not found!'
+        )
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'Done'
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Blog {id} not found'
+        )
+    blog.update(request)
+    db.commit()
+    return 'updated'
 
 
 @app.get('/blog')
