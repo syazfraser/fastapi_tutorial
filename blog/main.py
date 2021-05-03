4fda5da799b260f17f0a59ed955050dde70b6026
Syazwan Rifdi <syazwanrifdi@xsquare.sg>
2021-05-03 20:37:34 +0800

    Fixed AttributeError when doing put operation

--- blog/main.py
+++ blog/main.py
@@ -26,6 +26,12 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
+@app.get('/blog', response_model=List[schemas.ShowBlog])
+def get_all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
@@ -47,15 +53,9 @@ def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f'Blog {id} not found'
         )
-    blog.update(request)
+    blog.update(dict(request))
     db.commit()
     return 'updated'
-
-
-@app.get('/blog', response_model=List[schemas.ShowBlog])
-def get_all_blogs(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
 
 
 @app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
@@ -66,6 +66,59 @@ def get_single_blog(id, response: Response, db: Session = Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f'Blog {id} is not available.'
         )
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {'detail': f'Blog {id} is not available'}
     return blog
+
+
+@app.post('/user', status_code=status.HTTP_201_CREATED)
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get('/user', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowUser])
+def get_all_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
+
+@app.get('/user/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowUser)
+def get_single_user(id, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id)
+    if not user.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'User {id} not found!'
+        )
+    return user.first()
+
+
+@app.delete('/user/{id}')
+def delete_user(id, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'User with {id} not found'
+        )
+    user.delete(synchronize_session=False)
+    db.commit()
+    return f'User {id} deleted!'
+
+
+@app.put('/user/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_user(id, request: schemas.User, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id)
+    if not user.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'User {id} not found!'
+        )
+    user.update(dict(request))
+    db.commit()
+    return f'User {id} updated!'
+
+
+# # 2:23:10
