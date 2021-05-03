6b3cc8b4adf838a52dddda846af1ac4cdfe55353
Syazwan Rifdi <syazwanrifdi@xsquare.sg>
2021-05-03 10:49:04 +0800

    Trying out query parameters

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI
-
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'thisismyname'}}
+@app.get('/blog')
+def index(limit: int = 10, published: bool = True, sort: Optional[bool] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} unpublished blogs from the db'}
 
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blog/{id}')
+def show(id: int):
+    # Fetch blog with id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int, limit=10):
+    # Fetch comments of blog with id = id
+    return {'data': {id: {'comments': {'1', '2'}}}}
